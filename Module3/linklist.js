562509c7acc66a47737f0ae8a619de2c8bb04366
RownokNishat <rownokjahan900@gmail.com>
2025-11-05 15:58:33 +0600

    linklist

--- Module3/linklist.js
+++ Module3/linklist.js
@@ -4,10 +4,36 @@ class Node{
         this.next=null
     }
 }
-const head=new Node(10)
-head.next=new Node(20)
-let temp=head;
-while(!temp===null){
- console.log(temp.value,"")
- temp=temp.next
+// const head=new Node(10)
+// head.next=new Node(20)
+// console.log(head)
+// let temp=head;
+// console.log(temp)
+// while(temp!==null){
+//  console.log(temp.value,"")
+//  temp=temp.next
+// }
+
+class Linklist{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+    append(value){
+        const newnode=new Node(value)
+        if(this.head===null){
+            this.head=newnode;
+            this.tail=newnode
+        }
+        else{
+            this.tail.next=newnode
+            this.tail=newnode
+        }
+        this.length++
+    }
+    preppend(){}
+    append(){}
+    append(){}
+    append(){}
 }
